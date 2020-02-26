5919314ee465f2bb1192e2b2506de5d74b7a807b
Herbert Kociemba <kociemba@t-online.de>
2020-02-27 00:42:57 +0100

    Add files via upload

---
+++ pentacircles.dpr
@@ -0,0 +1,14 @@
+program pentacircles;
+
+uses
+  Vcl.Forms,
+  twoPentacircles in 'twoPentacircles.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
